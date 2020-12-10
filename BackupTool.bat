62ff3deedec4a9cff61c23df9f8b763c464f3cd3
XavierDupuis <XavierDupuis@users.noreply.github.com>
2020-12-09 20:10:13 -0500

    Dispatch, Mirror Copy

--- BackupTool.bat
+++ BackupTool.bat
@@ -1,84 +1,116 @@
 @echo off
-mode con:cols=50 lines=30
+mode con:cols=100 lines=20
 Title Backup
 
-echo ===================
-echo Proceed for Backup?
-echo ===================
+echo.
+echo ===================================================================================================
+echo =============          Proceed for Backup? (Default Backup Drive set to F:\)          =============
+echo ===================================================================================================
+echo.
 Pause
 
+:: SETTING COUNT OF DOMAINS
 set count=0
 
-if %TIME:~0,2% LSS 10 (set Hour=0%TIME:~1,1%) ELSE (set Hour=%TIME:~0,2%)
-set TimeStart=%Hour%.%TIME:~3,2%.%TIME:~6,2%
-echo Started : %date% %TimeStart%
-
-set BACKUPLOG=F:\BackupData
+:: SETTING BACKUP LOCATION AND LOG FOLDER LOCATION
+set DRIVE=F
+set BACKUPFOLDER=%DRIVE%:\!BACKUPDATA
+set BACKUPLOG=%BACKUPFOLDER%\!LOGS
+REM set BACKUPFOLDER=%cd%
 REM set BACKUPLOG=%cd%
 
-if not exist "%BACKUPLOG%" (
-echo No Backup location was found
-echo %Backup% not available
+:: CONFIRM BACKUP LOCATION && PROMPT RETRYING
+:DRIVEEXIST
+if exist "%BACKUPFOLDER%" (
+	goto BEGIN) ELSE (
+	cls
+	echo.
+	echo No Backup location was found
+	echo %BACKUPFOLDER:~0,3% drive not available
+	echo.
+	goto NEWDRIVE)
+:NEWDRIVE
+echo [Enter new drive letter]
+set /p DRIVE=
+set BACKUPFOLDER=%DRIVE%:\!BACKUPDATA
+set BACKUPLOG=%BACKUPFOLDER%\!LOGS
+echo.
 pause
-end)
+goto DRIVEEXIST
 
-MKDIR %BACKUPLOG%\Backup_%date%-[%TimeStart%]
-set BACKUPLOG=%BACKUPLOG%\Backup_%date%-[%TimeStart%]
+:BEGIN
+cls
+:: SETTING STARTING BACKUP TIME
+if %TIME:~0,2% LSS 10 (set Hour=0%TIME:~1,1%) ELSE (set Hour=%TIME:~0,2%)
+set TimeStart=%Hour%.%TIME:~3,2%.%TIME:~6,2%
+:: SETTING BACKUP LOG FILE LOCATION
+set LOGFILE="%BACKUPLOG%\Backup_%date%-[%TimeStart%].log"
 
+:: DISPATCHING TO DIFFERENT DOMAINS
 :DISPATCH
-echo %count%
-if %count%==0 goto MUSIC
-if %count%==1 goto DOCUMENTS
+REM echo %count%
+if %count%==0 set DOMAIN=DOCUMENTS
+if %count%==1 set DOMAIN=MUSIQUE
 if %count%==2 goto DONE
-
-:MUSIC
-set DOMAIN=Music
+set OUTPUTDIR="%BACKUPFOLDER%\[%DOMAIN%]"
 set /a count=%count%+1
-REM set INPUTDIR="C:\Users\Xavier\Music\iTunes\iTunes Media\Music"
-REM set OUTPUTDIR="F:\Musique\iTunes\iTunes Media\Music"
-set INPUTDIR="%cd%\1"
-set OUTPUTDIR="%cd%\2"
-goto COPYMIR
+goto :%DOMAIN%
 
 :DOCUMENTS
-set DOMAIN=Documents
-set /a count=%count%+1
 set INPUTDIR="C:\Users\Xavier\Documents"
-REM set OUTPUTDIR="F:\Desktop"
-REM set INPUTDIR="%cd%"
-set OUTPUTDIR="%cd%\3"
-goto COPY
+goto COPYMIRROR
 
+:MUSIQUE
+set INPUTDIR="C:\Users\Xavier\Music"
+goto COPYMIRROR
+
+:VIDEOS
+
+
+:: PURE COPY (ADDING TO BACKUP)
 :COPY
 Title Backing up %DOMAIN% (%count%/3)
-robocopy %INPUTDIR% %OUTPUTDIR% /E /XA:SH /W:1 /R:100 /ETA /TEE /XJ /X /V > "%BACKUPLOG%\Backup_%date%-[%TimeStart%]-%DOMAIN%.log"
+echo. >> %LOGFILE%
+echo =================================  %DOMAIN%  ================================== >> %LOGFILE%
+echo. >> %LOGFILE%
+robocopy %INPUTDIR% %OUTPUTDIR% /E /W:1 /R:100 /ETA /TEE /XJ /XJD /X /V /NJH /LOG+:%LOGFILE%
 goto DISPATCH
 
-:COPYMIR
+:: MIRROR COPY (REPLACING CURRENT BACKUP)
+:COPYMIRROR
 Title Backing up %DOMAIN% (%count%/3)
-robocopy %INPUTDIR% %OUTPUTDIR% /MIR /W:1 /R:100 /ETA /TEE /XJ /X /V > "%BACKUPLOG%\Backup_%date%-[%TimeStart%]-%DOMAIN%.log"
+echo. >> %LOGFILE%
+echo =================================  %DOMAIN%  ================================== >> %LOGFILE%
+echo. >> %LOGFILE%
+robocopy %INPUTDIR% %OUTPUTDIR% /MIR /W:1 /R:100 /ETA /TEE /XJ /XJD /X /V /NJH /LOG+:%LOGFILE%
 goto DISPATCH
 
+:: END
 :DONE
-
-pause
 cls
 echo.
 Title Backup Done
-echo.
-echo ===============================
 if %TIME:~0,2% LSS 10 (set Hour=0%TIME:~1,1%) ELSE (set Hour=%TIME:~0,2%)
 set TimeEnd=%Hour%.%TIME:~3,2%.%TIME:~6,2%
-echo Started   : %date% %TimeStart%
-echo Completed : %date% %TimeEnd%
-echo ===============================
+::TIME ELAPSED
+set /a SecondsElapsed=%TimeEnd:~6,2%-%TimeStart:~6,2%
+set /a MinutesElapsed=%TimeEnd:~3,2%-%TimeStart:~3,2%
+set /a HoursElapsed=%TimeEnd:~0,2%-%TimeStart:~0,2%
+if %SecondsElapsed% LSS 0 (
+	set /a SecondsElapsed=%SecondsElapsed%+60
+	set /a MinutesElapsed=%MinutesElapsed%-1)
+if %MinutesElapsed% LSS 0 (
+	set /a MinutesElapsed=%MinutesElapsed%+60
+	set /a HoursElapsed=%HoursElapsed%-1)
+if %HoursElapsed% LSS 0 (set /a HoursElapsed=%HoursElapsed%+24)
 echo.
-echo Log folder saved at "%BACKUPLOG%"
+echo ===================================================================================================
+echo Started    : %date% %TimeStart%
+echo Completed  : %date% %TimeEnd%
+echo Elapsed    : %HoursElapsed% hours %MinutesElapsed% minutes %SecondsElapsed% seconds 
+echo ===================================================================================================
 echo.
-
+echo Log file saved at "%BACKUPLOG%"
+echo.
 pause 
-
-RMDIR %OUTPUTDIR%
-RMDIR "%BACKUPLOG%\Backup_%date%-[%TimeStart%]-%DOMAIN%.log"
-
 exit
